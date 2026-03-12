d64d295a8c0d39d3a9629d29cbfa5959192ceaec
himanshu1029g <himanshu1029g@gmail.com>
2026-03-12 13:57:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// const express = require("express");
+
+import express from 'express'
+const app  = express()
+
+const PORT = process.env.PORT ?? 8080
+
+app.get("/", (req,res)=>{
+
+    return res.json({msg : "Hello from the server"})
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on  PORT : ${PORT}`)
+})
+
